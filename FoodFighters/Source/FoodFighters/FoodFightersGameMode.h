1882431470d5112969af41f63693a27e567b54d5
Electroshockist <ericonfire2@yahoo.com>
2019-06-11 20:09:44 -0400

    Added Starter Content Small assets

---
+++ FoodFighters/Source/FoodFighters/FoodFightersGameMode.h
@@ -0,0 +1,19 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FoodFightersGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AFoodFightersGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AFoodFightersGameMode();
+};
+
+
+
