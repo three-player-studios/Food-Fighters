3247188030097b59e1a28d31f49d6e2d7ef2b431
Raphael Rodriguez <32069809+FailureRate@users.noreply.github.com>
2019-10-08 12:23:25 -0400

    changes to the player BP

---
+++ FoodFighters/Source/FoodFighters/Interactable.h
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Interactable.generated.h"
+
+// This class does not need to be modified.
+UINTERFACE(MinimalAPI)
+class UInteractable : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+ * 
+ */
+class FOODFIGHTERS_API IInteractable
+{
+	GENERATED_BODY()
+
+	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
+public:
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category = "Interaction");
+	void OnInteract(AActor* Caller);
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category = "Interaction");
+	void StartFocus();
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category = "Interaction");
+	void EndFocus();
+};
