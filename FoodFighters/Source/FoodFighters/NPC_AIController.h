6a5b73871d565073e599917d347665dde0fcc800
Arifa tyrell <arifa.tyrell@gmail.com>
2020-02-03 00:42:43 -0500

    update ai
    continuing editinng the ai

--- FoodFighters/Source/FoodFighters/NPC_AIController.h
+++ FoodFighters/Source/FoodFighters/NPC_AIController.h
@@ -4,6 +4,16 @@
 
 #include "CoreMinimal.h"
 #include "AIController.h"
+#include "Perception/AIPerceptionComponent.h"
+#include "Perception/AISenseConfig_Sight.h"
+#include "Runtime/Engine/Classes/Kismet/GameplayStatics.h" 
+#include "Food.h"
+#include "NPCCharacter.h"
+#include "Waypoint.h"
+#include "Waypoint_Exit.h"
+#include "Waypoint_Checkout.h"
+#include "Waypoint_3.h"
+#include "Waypoint_2.h"
 #include "NPC_AIController.generated.h"
 
 /**
@@ -14,7 +24,52 @@ class FOODFIGHTERS_API ANPC_AIController : public AAIController
 {
 	GENERATED_BODY()
 	
-	
-	
+public:
+
+	ANPC_AIController();
+
+	virtual void BeginPlay() override;
+
+	//runs when ai controller is in the character
+	virtual void Possess(APawn* Pawn) override;
+
+	//run every frame
+	virtual void Tick(float DeltaSecounds) override;
+
+	// will alow ai Possess componeent to always point in the direction the character is facing in 
+	virtual FRotator GetControlRotation() const override;
+
+	// will dectecte if player is around 
+	UFUNCTION()
+		void OnFoodDectected(TArray<AActor*> DectectedPlayer);
+
+	// the radius of sight the ai bot can see 
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "AI")
+		float AIEyeRadius = 3500.0f;
+
+	// how long the ai will remeber the player 
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "AI")
+		float AIEyeMemroy = 5.0f;
+
+	// the radius of sight the ai bot can not see 
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "AI")
+		float AILostEyeRaduis = AIEyeRadius + 50.0f;
+
+	// angle of sight 
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "AI")
+		float AIEyeAngle = 90.0f;
+
+	// class that configres ai bot sight 
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "AI")
+		class UAISenseConfig_Sight* SightConfig;
+
+	// will dectecte if food is around or not 
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "AI")
+		bool IsTheFoodDetected = false;
+
+
+	//// tells the distance of the player 
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "AI")
+		float DistanceFromFood = 0.0f;
 	
 };
